89c335995ae08bca6d08dac41d37c6c9168cb72a
Jean-Claude Jung <jjung@adobe.com>
2020-06-05 12:36:28 +0200

    add case battery status

--- AirPods.widget/bt.py
+++ AirPods.widget/bt.py
@@ -5,38 +5,95 @@ import os
 import plistlib
 import sys
 
+
+class AirPod(object):
+    def __init__(self, address: str, name: str, left: str, right: str, case: str):
+        self.address = address
+        self.name = name
+        self.left = left
+        self.right = right
+        self.case = case
+
+
 # for debugging purpose defaults read /Library/Preferences/com.apple.Bluetooth
+def airpod_battery_status(device_id: str) -> tuple:
+    """
+    Get device status with a given address (MAC)
+
+    Args:
+
+        device_id (str): MAC address of the Device to search
 
 
-def airpod_battery_status(device_name):
+    Returns:
+
+        dict: Left/Right battery status
+
+    """
     with open("/Library/Preferences/com.apple.Bluetooth.plist", "rb") as f:
         pl = plistlib.load(f)
-    devices = pl.get("DeviceCache")
-    ret = ""
+    devices: dict = pl.get("DeviceCache")
+    ret: tuple = tuple()
     for d, v in devices.items():
-        if v.get("Name") and device_name in v.get("Name"):
-            right = v.get("BatteryPercentRight")
-            left = v.get("BatteryPercentLeft")
-            ret = '<br><span style="font-size: 8pt">L{0}% R{1}%</span>'.format(left, right)
+        if device_id in d:
+            right: str = v.get("BatteryPercentRight") if v.get("BatteryPercentRight") else None
+            left: str = v.get("BatteryPercentLeft") if v.get("BatteryPercentLeft") else None
+            case: str = v.get("BatteryPercentCase") if v.get("BatteryPercentCase") else None
+            ret = (left, right, case)
             break
     return ret
 
 
-HR = '<hr style="border:none;height:1px;background-color:lightgrey">'
+def airpods_connected() -> list:
+    """
+    Get Connected Airpods
+
+    Returns:
+
+        list: Return AirPod Device Object-list with address, name, left/right battery
+
+    """
+    jsn = json.loads(os.popen('/usr/local/bin/blueutil --paired --format json').read())
+    connected_aps: list = list()
+    for v in jsn:
+        name: str = v.get('name')
+        address: str = v.get('address')
+        if v.get('connected') and "AirPods" in v.get('name'):
+            left, right, case = airpod_battery_status(address)
+            ap = AirPod(address, name, left, right, case)
+            connected_aps.append(ap)
+    return connected_aps
+
+
+def device_strings() -> list:
+    """
+    Generates list of devices incl name and battery status of Airpods
+
+    Returns:
+
+        list: List of HTML strings
+
+    """
+    aps: list = airpods_connected()
+    devices = list()
+    for ap in aps:
+        left: str = f"L {ap.left}%" if ap.left else ""
+        right: str = f"R {ap.right}%" if ap.right else ""
+        case: str = f"C {ap.case}%" if ap.case else ""
+        name: str = ap.name
+        d_str = name
+        if left is not "" or right is not "":
+            d_str = f'{name}<br><span style="font-size: 8pt">{left} {right} {case}</span>'
+        devices.append(d_str)
+    return devices
+
+
 BT_IMG = '<img src = "AirPods.widget/airpod.png">'
 NO_BT_IMG = '<img src="AirPods.widget/noairpod.png">'
-jsn = json.loads(os.popen('/usr/local/bin/blueutil --paired --format json').read())
 
-#connected_list = ["{0} {1}".format(i.get('name'), airpod_battery_status(i.get('name'))) for i in jsn if i.get('connected') and "AirPods" in i.get('name')]
-# connected_list = [i.get("name") for i in jsn if i.get('connected') and "AirPods" in i.get('name')]
-connected_list = list()
-for v in jsn:
-    if v.get('connected') and "AirPods" in v.get('name'):
-        name = v.get('name')
-        ap_status = airpod_battery_status(name)
-        connected_list.append("{0} {1}".format(name, ap_status))
-connected = '<span style="color: grey">{0}AirPods not connected</span>'.format(NO_BT_IMG)
+connected_list = device_strings()
+connected = f'<span style="color: grey">{NO_BT_IMG}AirPods not connected</span>'
 if len(connected_list) > 0:
     connected = "<br>".join(connected_list)
-    connected = BT_IMG + connected
+    connected = f"{BT_IMG}{connected}"
 sys.stdout.write(connected)
