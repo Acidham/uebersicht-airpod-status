0f1aabb13085df1d05f2ff377d3f42f80fdccd20
Acidham <acidham@gmail.com>
2020-12-04 13:04:52 +0100

    Fixed operating error #3

--- AirPods.widget/bt.py
+++ AirPods.widget/bt.py
@@ -11,12 +11,12 @@ AIRPD_PRODUCT_INDX = {
     8206: "airpodpro",
     8194: "airpod1",
     8207: "airpod2"
-
 }
 
 
 class AirPod:
-    def __init__(self, address: str, name: str, left: str, right: str, case: str, product_id: int, vendor_id: int, product: str = ""):
+    def __init__(self, ap: tuple):
+        address, name, left, right, case, product_id, vendor_id = ap
         self.address = address
         self.name = name
         self.left = left
@@ -28,7 +28,7 @@ class AirPod:
 
 
 # for debugging purpose: defaults read /Library/Preferences/com.apple.Bluetooth
-def airpod_battery_status(device_id: str) -> tuple:
+def airpod_info(device_id: str) -> tuple:
     """
     Get device status with a given address (MAC)
 
@@ -44,17 +44,20 @@ def airpod_battery_status(device_id: str) -> tuple:
     ret: tuple = tuple()
     for d, v in devices.items():
         if device_id in d:
-            right: str = v.get("BatteryPercentRight") if v.get("BatteryPercentRight") else None
-            left: str = v.get("BatteryPercentLeft") if v.get("BatteryPercentLeft") else None
-            case: str = v.get("BatteryPercentCase") if v.get("BatteryPercentCase") else None
-            product_id: str = v.get("ProductID") if v.get("ProductID") else None
-            vendor_id: str = v.get("VendorID") if v.get("VendorID") else None
-            ret = (left, right, case, product_id, vendor_id)
+            ret = (
+                d,  # address
+                v.get("Name"),
+                v.get("BatteryPercentLeft"),
+                v.get("BatteryPercentRight"),
+                v.get("BatteryPercentCase"),
+                v.get("ProductID"),
+                v.get("VendorID")
+            )
             break
     return ret
 
 
-def airpods_connected() -> list:
+def paired_airpods() -> list:
     """
     Get list of connected AirPods
 
@@ -67,11 +70,10 @@ def airpods_connected() -> list:
         name: str = v.get('name')
         address: str = v.get('address')
         if v.get('connected'):
-            left, right, case, product_id, vendor_id = airpod_battery_status(address)
-            ap = AirPod(address, name, left, right, case, product_id, vendor_id)
+            airpod = AirPod(airpod_info(address))
             # 76: Apple; 8206: AirPods Pro; 8194: AirPods 1; 8207: AirPods 2
-            if ap.vendor_id == 76 and ap.product_id in AIRPD_PRODUCT_INDX:
-                connected_aps.append(ap)
+            if airpod.vendor_id == 76 and airpod.product_id in AIRPD_PRODUCT_INDX:
+                connected_aps.append(airpod)
     return connected_aps
 
 
@@ -83,16 +85,16 @@ def get_device_html() -> list:
         list: List of HTML strings
 
     """
-    aps: list = airpods_connected()
+    airpods: list = paired_airpods()
     devices = list()
-    for ap in aps:
+    for ap in airpods:
         left: str = f"L {ap.left}%" if ap.left else ""
         right: str = f"R {ap.right}%" if ap.right else ""
         case: str = f"C {ap.case}%" if ap.case else ""
         product: str = f'{ap.product}'
         name: str = ap.name
         d_str = name
-        if left is not "" or right is not "":
+        if left != "" or right != "":
             d_str = f"""
             <img src="{ICON_PATH}{product}.png">
             <div>{name}</div><span class="s-txt">{left} {right} {case}</span>
@@ -103,7 +105,7 @@ def get_device_html() -> list:
 
 def main():
     connected_list = get_device_html()
-    connected = f'<div class="s-box">{NO_BT_IMG}<div style="color: grey">AirPods not connected</div></div>'
+    connected = f'<div class="s-box">{NO_BT_IMG}<div style="font-size: 11pt;color: grey">AirPods not connected</div></div>'
     if len(connected_list) > 0:
         connected = "<br>".join(connected_list)
     sys.stdout.write(connected)
